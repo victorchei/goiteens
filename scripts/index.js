4cb8108b87fef05fb18e8b276442dcdacaa87370
Viktor Zhelizko <viktor.zhelizko@infopulse.com>
2024-05-14 11:34:24 +0300

    promise methods example

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 // export * from './date-example.js'
 // export * from './async-example.js'
-export * from './promise-pr.js'
+// export * from './promise-pr.js'
+export * from "./promise-methods.js";
 
 // console.log("index.js works");
